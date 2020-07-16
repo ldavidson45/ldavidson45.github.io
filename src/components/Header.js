fcc75a338d881ac1ddcaf1a29ea06c18ddf41095
Liz Davidson <lizdavidson45@gmail.com>
2020-07-16 12:00:14 -0400

    Initial setup with react

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Header.scss"
+
+function Header() {
+	return (
+		<header>
+			<div class="header">
+				<h1 className="header__title">Hi, I'm Liz.</h1>
+				<p className="header__subtitle">
+					I'm a front-end web developer.
+				</p>
+			</div>
+		</header>
+	)
+}
+
+export default Header
